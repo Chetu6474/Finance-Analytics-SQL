87e35f68fca5f966637b2d9d3f63543ebf07a12d
Chetu6474 <126118586+Chetu6474@users.noreply.github.com>
2025-02-06 13:48:26 -0500

    Delete get_fiscal_year.sql

--- get_fiscal_year.sql
+++
@@ -1,9 +0,0 @@
-CREATE DEFINER=`root`@`localhost` FUNCTION `get_fiscal_year`(
-calendar_date date
-) RETURNS int
-    DETERMINISTIC
-BEGIN
-  declare fiscal_year INT;
-  set fiscal_year = YEAR(DATE_ADD(calendar_date,INTERVAL 4 MONTH));
-  return fiscal_year;
-END
